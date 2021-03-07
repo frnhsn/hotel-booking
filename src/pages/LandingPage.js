11905309f57483546656b10a3969e8a262eb482b
Farhan <saturnv@Farhans-MacBook-Air.local>
2021-03-07 15:14:56 +0700

    balikin index.js

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from 'pages/landingPage';
+
+export default function landingPage() {
+    return (
+        <div>
+            <Header {...this.props}></Header>
+        </div>
+    )
+}
+
